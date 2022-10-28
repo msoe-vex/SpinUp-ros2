47e5fa020bee837d8b9cfcf67ff01b757e479312
goetschm <goetschm@msoe.edu>
2022-10-27 21:22:57 -0500

    make m_fieldOrientedControl() public

--- include/nodes/subsystems/HolonomicDriveNode.h
+++ include/nodes/subsystems/HolonomicDriveNode.h
@@ -1,86 +1,91 @@
 #pragma once
 
-#include <algorithm>
-#include <initializer_list>
-#include "nodes/NodeManager.h"
 #include "api.h"
-#include "nodes/subsystems/IDriveNode.h"
+#include "eigen/Eigen/Dense"
+#include "kinematics/HolonomicDriveKinematics.h"
+#include "nodes/NodeManager.h"
 #include "nodes/actuator_nodes/MotorNode.h"
 #include "nodes/sensor_nodes/ControllerNode.h"
 #include "nodes/sensor_nodes/InertialSensorNode.h"
-#include "kinematics/HolonomicDriveKinematics.h"
-#include "eigen/Eigen/Dense"
+#include "nodes/subsystems/IDriveNode.h"
+#include <algorithm>
+#include <initializer_list>
+
 
 class HolonomicDriveNode : public IDriveNode {
-public: 
-    struct HolonomicEightMotors {
-        MotorNode* left_front_motor;
-        MotorNode* left_front_motor_2;
-        MotorNode* left_rear_motor;
-        MotorNode* left_rear_motor_2;
-        MotorNode* right_front_motor;
-        MotorNode* right_front_motor_2;
-        MotorNode* right_rear_motor;
-        MotorNode* right_rear_motor_2;
-    };
+public:
+  struct HolonomicEightMotors {
+    MotorNode *left_front_motor;
+    MotorNode *left_front_motor_2;
+    MotorNode *left_rear_motor;
+    MotorNode *left_rear_motor_2;
+    MotorNode *right_front_motor;
+    MotorNode *right_front_motor_2;
+    MotorNode *right_rear_motor;
+    MotorNode *right_rear_motor_2;
+  };
 
-    HolonomicDriveNode(NodeManager* node_manager, std::string handle_name, ControllerNode* controller, 
-        InertialSensorNode* inertial_sensor, HolonomicEightMotors motors, HolonomicDriveKinematics kinematics);
+  HolonomicDriveNode(NodeManager *node_manager, std::string handle_name,
+                     ControllerNode *controller,
+                     InertialSensorNode *inertial_sensor,
+                     HolonomicEightMotors motors,
+                     HolonomicDriveKinematics kinematics);
 
-    void initialize();
+  void initialize();
 
-    void resetEncoders();
+  void resetEncoders();
 
-    IDriveNode::FourMotorDriveEncoderVals getIntegratedEncoderVals();
+  IDriveNode::FourMotorDriveEncoderVals getIntegratedEncoderVals();
 
-    void setDriveVoltage(int x_voltage, int theta_voltage);
+  void setDriveVoltage(int x_voltage, int theta_voltage);
 
-    void setDriveVoltage(int x_voltage, int y_voltage, int theta_voltage);
+  void setDriveVoltage(int x_voltage, int y_voltage, int theta_voltage);
 
-    void setDriveVoltage(int left_x, int left_y, int right_x, int right_y);
+  void setDriveVoltage(int left_x, int left_y, int right_x, int right_y);
 
-    void setDriveVelocity(float x_velocity, float theta_velocity);
+  void setDriveVelocity(float x_velocity, float theta_velocity);
 
-    void setDriveVelocity(float x_velocity, float y_velocity, float theta_velocity);
+  void setDriveVelocity(float x_velocity, float y_velocity,
+                        float theta_velocity);
 
-    void teleopPeriodic();
+  void teleopPeriodic();
 
-    void autonPeriodic();
+  void autonPeriodic();
 
-    ~HolonomicDriveNode();
+  void m_fieldOrientedControl();
+
+  ~HolonomicDriveNode();
 
 private:
-    pros::Controller* m_controller;
+  pros::Controller *m_controller;
 
-    InertialSensorNode* m_inertial_sensor;
+  InertialSensorNode *m_inertial_sensor;
 
-    std::string m_handle_name;
+  std::string m_handle_name;
 
-    HolonomicEightMotors m_motors;
+  HolonomicEightMotors m_motors;
 
-    HolonomicDriveKinematics m_kinematics;
+  HolonomicDriveKinematics m_kinematics;
 
-    Eigen::Vector2d controller_target_velocity;
-    Eigen::Vector2d field_target_velocity;
-    double rotation_velocity;
+  Eigen::Vector2d controller_target_velocity;
+  Eigen::Vector2d field_target_velocity;
+  double rotation_velocity;
 
-    void m_setLeftFrontVoltage(int voltage);
+  void m_setLeftFrontVoltage(int voltage);
 
-    void m_setLeftRearVoltage(int voltage);
+  void m_setLeftRearVoltage(int voltage);
 
-    void m_setRightFrontVoltage(int voltage);
+  void m_setRightFrontVoltage(int voltage);
 
-    void m_setRightRearVoltage(int voltage);
+  void m_setRightRearVoltage(int voltage);
 
-    void m_setLeftFrontVelocity(float velocity);
+  void m_setLeftFrontVelocity(float velocity);
 
-    void m_setLeftRearVelocity(float velocity);
+  void m_setLeftRearVelocity(float velocity);
 
-    void m_setRightFrontVelocity(float velocity);
+  void m_setRightFrontVelocity(float velocity);
 
-    void m_setRightRearVelocity(float velocity);
+  void m_setRightRearVelocity(float velocity);
 
-    void m_fieldOrientedControl();
-
-    void m_tankControl();
+  void m_tankControl();
 };
