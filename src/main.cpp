62c2847a1996b58bca31059ad95bd1a1e1e74fc4
VRSD5 <vanzante@msoe.edu>
2022-11-04 09:18:31 -0500

    Made driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,18 @@
 #include "main.h"
 
+
+
 NodeManager* nodeManager;
 
 NodeManager* node_manager = new NodeManager(pros::millis);
 
 ControllerNode* primary_controller;
 
-MotorNode* left_front_drive;
-MotorNode* left_front_drive_2;
-MotorNode* left_rear_drive;
-MotorNode* left_rear_drive_2;
-MotorNode* right_front_drive;
-MotorNode* right_front_drive_2;
-MotorNode* right_rear_drive;
-MotorNode* right_rear_drive_2;
-HolonomicDriveNode* holonomic_drive_node;
+MotorNode* motor_left;
+MotorNode* motor_right;
+FlyWheelNode* flyWheel;
 
-InertialSensorNode* inertial_sensor;
+
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -25,38 +21,16 @@ InertialSensorNode* inertial_sensor;
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
+    pros::lcd::initialize();
+
 	nodeManager = new NodeManager(pros::millis);
+    primary_controller = new ControllerNode(node_manager, "primary");
 
-	HolonomicDriveNode::HolonomicEightMotors holonomic_drive_motors = {
-		left_front_drive, 
-		left_front_drive_2,
-		left_rear_drive,
-		left_rear_drive_2, 
-		right_front_drive,
-		right_front_drive_2,
-		right_rear_drive,
-		right_rear_drive_2 
-	};
+    motor_left = new MotorNode(nodeManager, 11, "shooter_motor_left");
+    motor_right = new MotorNode(nodeManager, 12, "fly_wheel_motor_right");
+    motor_left->getMotor()->get_actual_velocity();
 
-	EncoderConfig holonomic_encoder_config = {
-		0, // Initial ticks
-		360, // Ticks per RPM
-		3.75 // Wheel diameter
-	};
-
-	HolonomicDriveKinematics::HolonomicWheelLocations holonomic_wheel_locations = {
-		Vector2d(-5.48, 5.48), // Left front
-		Vector2d(-5.48, -5.48), // Left rear
-		Vector2d(5.48, 5.48), // Right front
-		Vector2d(5.48, -5.48) // Right rear
-	};
-
-	HolonomicDriveKinematics holonomic_drive_kinematics(holonomic_encoder_config, holonomic_wheel_locations);
-
-    holonomic_drive_node = new HolonomicDriveNode(node_manager, "drivetrain", primary_controller, inertial_sensor,
-	    holonomic_drive_motors,	holonomic_drive_kinematics);
-
-
+    flyWheel = new FlyWheelNode(nodeManager, "fly_wheel", primary_controller, motor_left, motor_right);
 
 }
 
@@ -111,5 +85,6 @@ void opcontrol() {
 	// Execute teleop code
 	while (true) {
 		nodeManager->executeTeleop();
+        flyWheel->teleopPeriodic();
 	}
 }
