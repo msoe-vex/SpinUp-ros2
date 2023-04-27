02e0d0929af149c3ac0ef9b8dc3dfac3fc8ab279
Andy Dao <andydao2020@gmail.com>
2023-04-27 16:55:53 -0500

    Setting up butteryfly functionality

---
+++ include/nodes/subsystems/ButterflyNode.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "nodes/NodeManager.h"
+#include "nodes/subsystems/IClawNode.h"
+#include "nodes/sensor_nodes/ControllerNode.h"
+#include "nodes/actuator_nodes/ADIDigitalOutNode.h"
+#include "pros/misc.h"
+
+class ButterflyNode : public IClawNode {
+private:
+    ControllerNode* m_controller;
+    ADIDigitalOutNode* m_claw;
+    bool m_claw_open = false;
+    bool m_a_previous_state = false;
+    pros::controller_digital_e_t m_button;
+
+public:
+    ButterflyNode(NodeManager* node_manager, std::string handle_name, 
+    ControllerNode* controller, ADIDigitalOutNode* claw, pros::controller_digital_e_t button);
+
+    void initialize();
+
+    void setState(bool opened);
+
+    void teleopPeriodic();
+
+    void autonPeriodic();
+
+    ~ButterflyNode();
+};
---
+++ src/nodes/subsystems/ButterflyNode.cpp
@@ -0,0 +1,44 @@
+#include "nodes/subsystems/ButterflyNode.h"
+
+ButterflyNode::ButterflyNode(NodeManager* node_manager, std::string handle_name, 
+		ControllerNode* controller, ADIDigitalOutNode* claw, pros::controller_digital_e_t button) : 
+		IClawNode(node_manager, handle_name), 
+		m_controller(controller),
+		m_claw(claw),
+		m_button(button),
+		m_claw_open(true),
+		m_a_previous_state(false){
+
+}
+
+void ButterflyNode::initialize() {
+
+}
+
+void ButterflyNode::setState(bool opened) {
+	if (opened) {
+		m_claw->setValue(0);
+	} else {
+        m_claw->setValue(1);
+	}
+}
+
+void ButterflyNode::teleopPeriodic() {
+	bool a_current_state = m_controller->getController()->get_digital(m_button);
+	//setState(!a_current_state);
+
+	if (a_current_state == 1 && m_a_previous_state == 0) {
+		m_claw_open = !m_claw_open;
+		setState(m_claw_open);
+	}
+
+	m_a_previous_state = a_current_state;
+}
+
+void ButterflyNode::autonPeriodic() {
+
+}
+
+ButterflyNode::~ButterflyNode() {
+
+}
